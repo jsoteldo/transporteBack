05b2bc50abf501c4aca00097c1fc1f772f50e4c0
jsoteldo <georsot@gmail.com>
2024-04-26 06:57:04 -0500

    mod

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ import paisRoutes from './routes/geolocalidad/pais-routes.js';
 import usersRoutes from './routes/users-routes.js';
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(productosRoutes,
